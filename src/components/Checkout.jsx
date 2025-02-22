dc7133d21c848d13b9feea4fad8fc1c2c5cb3155
BrajsundariRathor <rathorbrajsundari@gmail.com>
2025-02-22 16:04:33 +0530

    add checkout page

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,10 +1,31 @@
+import { placeOrder } from '@/redux/action';
+import { VStack, Text, Button } from '@chakra-ui/react';
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Checkout = () => {
+
+    const dispatch = useDispatch();
+    const cartItmes = useSelector((state) => state.cart.items);
+    const totalPrice = cartItmes.reduce((total, item) => total + item.price * item.quantity, 0);
+
+    const handlePlaceOrder = () => {
+        dispatch(placeOrder());
+    };
+
   return (
-    <div>
-      Checkout
-    </div>
+    <VStack gap={4} p={4}>
+        <Text fontSize={'2xl'}> Order Summary</Text>
+        {cartItmes.map((item) => (
+            <Text key={item.id}> {item.title} - {item.quantity} x ${item.price} </Text>
+        ))}
+        <Text fontSize={'xl'}>Total : ${totalPrice.toFixed(2)} </Text>
+        <Button colorPalette={'blue'} onClick={handlePlaceOrder}>
+            <Link to='/order-success'>Place Order</Link>
+        </Button>
+        
+    </VStack>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider as ChakraProvider } from './components/ui/provider'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 // import './index.css'
 import App from './App.jsx'
@@ -11,9 +11,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <ChakraProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </ChakraProvider>
     </Provider>
 
