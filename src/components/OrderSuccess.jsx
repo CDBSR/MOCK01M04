3a8f52cf8121cf021fcb5dc3432abafdd91eac74
BrajsundariRathor <rathorbrajsundari@gmail.com>
2025-02-22 16:12:43 +0530

    add OrderPlaced page

--- src/components/OrderSuccess.jsx
+++ src/components/OrderSuccess.jsx
@@ -1,10 +1,15 @@
+import { VStack, Text, Button } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const OrderSuccess = () => {
   return (
-    <div>
-      OderSuccess
-    </div>
+    <VStack gap={4} p={4}>
+        <Text fontSize={'2xl'}>Order Placed Successfully</Text>
+        <Link to='/'>
+            <Button colorPalette={'blue'}>Continue Shopping</Button>
+        </Link>
+    </VStack>
   )
 }
 
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -6,31 +6,36 @@ import { useDispatch, useSelector } from 'react-redux'
 const ProductList = () => {
 
     const dispatch = useDispatch();
-    const {products, loading, error} = useSelector((state) => state.products);
+    const { products, loading, error } = useSelector((state) => state.products);
 
     useEffect(() => {
         dispatch(fetchProducts());
     }, [dispatch]);
 
-    if(loading) {
+    if (loading) {
         return <Text>Loading...</Text>
     }
-    if(error) {
+    if (error) {
         return <Text>Error: {error} </Text>
     }
 
-  return (
-    <SimpleGrid columns={3} gap={10} p={4}>
-        {products.map((product) => (
-            <Box key={product.id} borderWidth='1px' borderRadius='lg' p={4} alignItems={'center'} textAlign={'center'}>
-                <Image src={product.image} alt={product.title} boxSize={'350px'} objectFit={'contain'}></Image>
-                <Text fontSize={'xl'} fontWeight={'bold'}> {product.title} </Text>
-                <Text fontSize={'lg'}> ${product.price} </Text>
-                <Button colorPalette={'blue'} onClick={() => dispatch(addToCart(product))}>Add To Cart</Button>
-            </Box>
-        ))}
-    </SimpleGrid>
-  )
+    return (
+        <SimpleGrid columns={3} gap={10} p={4}>
+            {products.map((product) => (
+                <Box key={product.id} borderWidth='1px' borderRadius='lg' p={4} alignItems={'center'} textAlign={'center'}>
+                    <Box maxWidth={'350px'} margin={'auto'} overflow={'hidden'}>
+                        <Image src={product.image} alt={product.title} width={'300px'}></Image>
+                    </Box>
+                    <Box mt={4} maxWidth={'300px'} margin={'5px'}>
+                        <Text fontSize={'xl'} fontWeight={'bold'}> {product.title} </Text>
+                        <Text fontSize={'lg'}> ${product.price} </Text>
+                    </Box>
+
+                    <Button mt={5} colorPalette={'blue'} onClick={() => dispatch(addToCart(product))}>Add To Cart</Button>
+                </Box>
+            ))}
+        </SimpleGrid>
+    )
 }
 
 export default ProductList
